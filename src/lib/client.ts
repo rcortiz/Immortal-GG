318ff06bd426161b3b3aaa2efc10f88617bad196
rcortiz <rcortiz.dev@gmail.com>
2024-12-26 20:31:05 +0800

    feat: add error logs in apollo client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,17 +1,33 @@
 "use client";
 
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+} from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support";
+
+const errorLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    if (response.errors) {
+      console.error("GraphQL Errors:", response.errors);
+    }
+    return response;
+  });
+});
+
+const httpLink = new HttpLink({
+  uri: "https://api.stratz.com/graphiql",
+  headers: {
+    Authorization: `Bearer ${process.env.STRATZ_API_KEY}`,
+    "User-Agent": "STRATZ_API",
+  },
+});
 
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-      uri: "https://api.stratz.com/graphiql",
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRATZ_API_KEY}`,
-        "User-Agent": "STRATZ_API",
-      },
-    }),
+    link: ApolloLink.from([errorLink, httpLink]),
   });
 });
