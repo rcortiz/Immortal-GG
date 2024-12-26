5bf59902b4c985d0ddc5dd09371946b9f185d1c6
rcortiz <rcortiz.dev@gmail.com>
2024-12-26 20:07:52 +0800

    fix: env variables format

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,31 +4,18 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support";
 
 export const { getClient } = registerApolloClient(() => {
+  const isServer = typeof window === "undefined";
+
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
       uri: "https://api.stratz.com/graphiql",
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRATZ_API_KEY}`,
-        "User-Agent": "STRATZ_API",
-      },
+      headers: isServer
+        ? {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRATZ_API_KEY}`,
+            "User-Agent": "STRATZ_API",
+          }
+        : {},
     }),
   });
 });
-
-// "use client";
-
-// import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-
-// const client = new ApolloClient({
-//   cache: new InMemoryCache(),
-//   link: new HttpLink({
-//     uri: "https://api.stratz.com/graphiql",
-//     headers: {
-//       Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRATZ_API_KEY}`,
-//       "User-Agent": "STRATZ_API",
-//     },
-//   }),
-// });
-
-// export default client;
